b02f71eb882b1804090f39e3c3bb84937496b182
0Trickster0 <37331560+0Trickster0@users.noreply.github.com>
2018-08-06 18:40:02 -0400

    add ui

--- Baby Smash/Assets/Scripts/PlayerManager.cs
+++ Baby Smash/Assets/Scripts/PlayerManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
 
     public float scoreP1;
     public float scoreP2;
+    public Text scoreP1Text;
+    public Text scoreP2Text;
 
     private static PlayerManager instance;
     public static PlayerManager Instance
@@ -32,6 +35,7 @@ public class PlayerManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        scoreP1Text.text = "Player1 Score:" + scoreP1.ToString();
+        scoreP2Text.text = "Player2 Score:" + scoreP2.ToString();
+    }
 }
