3ce1fac40ba422570abeb053e2c6bb9a3d788010
0Trickster0 <37331560+0Trickster0@users.noreply.github.com>
2018-08-06 15:28:25 -0400

    add spring

---
+++ Baby Smash/Assets/Scripts/Spring.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+
+    public float springThrust;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.collider.SendMessage("BeSpringed", springThrust);
+        }
+    }
+}
