54bd01574340793c2bb246dd66262e09ad957ad3
Kozibear <alexander.kozina@gmail.com>
2018-08-07 15:35:02 -0400

    Added timer script and cracked window

---
+++ Baby Smash/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public float timeLeft;
+
+	public Text timeLeftText;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(TimeLoop());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeLeftText.text = "Time: " + timeLeft;
+
+		if (timeLeft == 0) {
+			print ("end");
+		}
+	}
+
+	private IEnumerator TimeLoop()
+	{
+		while (timeLeft > 0) {
+			yield return new WaitForSeconds (1);
+			timeLeft -= 1;
+			print (timeLeft);
+		}
+	}
+}
