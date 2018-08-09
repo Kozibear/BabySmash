ecf4d9effb40924eb00532bc1e5512cee02efa19
Kozibear <alexander.kozina@gmail.com>
2018-08-09 13:17:28 -0400

    Fully Implemented Credits Screen

---
+++ Baby Smash/Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsButton : MonoBehaviour {
+
+	public GameObject CreditsScreen;
+
+	public Button credits;
+
+	// Use this for initialization
+	void Start () {
+		credits.onClick.AddListener (credits1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void credits1 () {
+		CreditsScreen.SetActive (true);
+	}
+}
